43698731dfa90b0af9a21b7735020f01d91d455b
병선 Byungsun <57519989+logan-jun@users.noreply.github.com>
2020-03-04 16:45:34 +0900

    test

--- dags/airflowlib/emr_lib.py
+++ dags/airflowlib/emr_lib.py
@@ -34,8 +34,8 @@ def add_job_flow_steps(jobflowId):
     return client_response['StepIds']
 
 def create_cluster(region_name, cluster_name='Airflow-' + str(datetime.now()), release_label='emr-5.9.0',master_instance_type='m3.xlarge', num_core_nodes=2, core_node_instance_type='m3.2xlarge'):
-    emr_master_security_group_id = get_security_group_id('AirflowEMRMasterSG', region_name=region_name)
-    emr_slave_security_group_id = get_security_group_id('AirflowEMRSlaveSG', region_name=region_name)
+    emr_master_security_group_id = get_security_group_id('AirflowEMRMasterSG3', region_name=region_name)
+    emr_slave_security_group_id = get_security_group_id('AirflowEMRSlaveSG3', region_name=region_name)
     cluster_response = emr.run_job_flow(
         Name=cluster_name,
         ReleaseLabel=release_label,
@@ -62,8 +62,8 @@ def create_cluster(region_name, cluster_name='Airflow-' + str(datetime.now()), r
             'EmrManagedSlaveSecurityGroup': emr_slave_security_group_id
         },
         VisibleToAllUsers=True,
-        JobFlowRole='EmrEc2InstanceProfile',
-        ServiceRole='EmrRole',
+        JobFlowRole='EmrEc2InstanceProfile3',
+        ServiceRole='EmrRole3',
         Applications=[
             { 'Name': 'hadoop' },
             { 'Name': 'spark' },
