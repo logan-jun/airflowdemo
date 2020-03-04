ae59d93ba4b3675f564b08a3a58967992cae448c
병선 Byungsun <57519989+logan-jun@users.noreply.github.com>
2020-03-04 11:47:47 +0900

    initial commit

---
+++ dags/beam_dag.py
@@ -0,0 +1,70 @@
+import airflowlib.emr_lib as emr
+import os
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2017, 11, 01),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=2),
+    'provide_context': True
+}
+
+# Initialize the DAG
+# Concurrency --> Number of tasks allowed to run concurrently
+dag = DAG('transform_movielens', concurrency=3, schedule_interval=None, default_args=default_args)
+region = emr.get_region()
+emr.client(region_name=region)
+
+# Creates an EMR cluster
+def create_emr(**kwargs):
+    cluster_id = emr.create_cluster(region_name=region, cluster_name='beam_cluster1', num_core_nodes=2)
+    return cluster_id
+
+# Creates a step in EMR cluster
+def create_step(**kwargs):
+    cluster_id = ti.xcom_pull(task_ids='create_cluster')
+    step_id = emr.add_job_flow_steps(jobflowId=cluster_id)
+    return step_ids
+
+# Waits for the EMR cluster to be ready to accept jobs
+def wait_for_completion(**kwargs):
+    ti = kwargs['ti']
+    cluster_id = ti.xcom_pull(task_ids='create_cluster')
+    emr.wait_for_cluster_creation(cluster_id)
+
+# Terminates the EMR cluster
+def terminate_emr(**kwargs):
+    ti = kwargs['ti']
+    cluster_id = ti.xcom_pull(task_ids='create_cluster')
+    emr.terminate_cluster(cluster_id)
+
+# Define the individual tasks using Python Operators
+create_cluster = PythonOperator(
+    task_id='create_cluster',
+    python_callable=create_emr,
+    dag=dag)
+
+add_emr_step = PythonOperator(
+    task_id='add_emr_step',
+    python_callable=create_step,
+    dag=dag)
+
+wait_for_cluster_completion = PythonOperator(
+    task_id='wait_for_cluster_completion',
+    python_callable=wait_for_completion,
+    dag=dag)
+
+terminate_cluster = PythonOperator(
+    task_id='terminate_cluster',
+    python_callable=terminate_emr,
+    trigger_rule='all_done',
+    dag=dag)
+
+# construct the DAG by setting the dependencies
+create_cluster >> wait_for_cluster_completion
+wait_for_cluster_completion >> add_emr_step >> terminate_cluster
