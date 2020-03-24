efb4e2396bca35f4c93ff122d3633885b42f5c52
병선 Byungsun <57519989+logan-jun@users.noreply.github.com>
2020-03-24 16:44:07 +0900

    test

--- dags/airflowlib/gcs_lib.py
+++ dags/airflowlib/gcs_lib.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 # simple download task
 def download_file_gcs(bucket, filename, destination):
-    storage_client = storage.Client.from_service_account_json('/root/airflow/sourcecode/airflowdemo/auth/ds-data-platform-f859ac098f78.json')
+    storage_client = storage.Client.from_service_account_json('/root/airflow/auth/ds-data-platform-f859ac098f78.json')
     bucket = storage_client.get_bucket(bucket)
     blob = bucket.blob(filename)
     return blob.download_to_filename(destination)
