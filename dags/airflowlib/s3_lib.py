7e75e20f2f3511a1c3ac0be277c08918912f53c2
병선 Byungsun <57519989+logan-jun@users.noreply.github.com>
2020-03-24 16:01:49 +0900

    test

--- dags/airflowlib/s3_lib.py
+++ dags/airflowlib/s3_lib.py
@@ -1,17 +1,19 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
+
 from datetime import datetime, timedelta
 
+
 # simple download task
-def download_file_s3(bucket, key, destination):
+def download_file(bucket, key, destination):
     import boto3
     s3 = boto3.resource('s3')
     return s3.meta.client.download_file(bucket, key, destination)
 
 
 # simple upload task
-def upload_file_s3(source, bucket, key):
+def upload_file(source, bucket, key):
     import boto3
     s3 = boto3.resource('s3')
     return s3.Bucket(bucket).upload_file(source, key)
